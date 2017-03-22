27344c3f191d7b7eb271247756dbdf1668931c15
Steven Bergner <git-steb@users.noreply.github.com>
2017-03-21 22:08:15 -0700

    Switched sensor flow architecture to use shared_ptr
    	* resolved segfault in release version
    	* build it using: cd src; make release

---
+++ src/types_fwd.h
@@ -0,0 +1,23 @@
+/* -*- C++ -*- */
+#ifndef __TYPES_FWD_H__
+#define __TYPES_FWD_H__
+
+/** \file types_fwd.h
+ * Forward declarations of shared pointers.
+ */
+
+#include <memory>
+
+class Model;
+typedef std::shared_ptr<Model> model_ptr;
+typedef std::shared_ptr<const Model> model_cptr;
+
+class Sensor;
+typedef std::shared_ptr<Sensor> sensor_ptr;
+typedef std::shared_ptr<const Sensor> sensor_cptr;
+
+class Dataset;
+typedef std::shared_ptr<Dataset> dataset_ptr;
+typedef std::shared_ptr<const Dataset> dataset_cptr;
+
+#endif
